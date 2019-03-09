037abe12f4011d25a93e109ed848762f16a36231
Kyle Graden <kylegraden3@gmail.com>
2019-03-09 02:25:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,8 +38,8 @@ def using_flatten(array)
   array.flatten()
 end
 
-def using_delete(array)
-  array.delete()
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete(array, int)
